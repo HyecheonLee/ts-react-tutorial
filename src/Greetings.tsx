aaa011870ef51d8322f7632deadd697114fe2204
hyecheon <rainbow880616@gmail.com>
2019-12-22 13:45:40 +0900

    타입스크립트 리액트 컴포넌트

---
+++ src/Greetings.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+type GreetingsProps = {
+  name: string;
+  mark?: string;
+  onClick: (name: string) => void;
+};
+
+function Greetings({ name, mark, onClick }: GreetingsProps) {
+  const handleClick = () => onClick(name);
+  return (
+    <div>
+      Hello, {name}! {mark}
+      <div>
+        <button onClick={handleClick}></button>
+      </div>
+    </div>
+  );
+}
+
+export default Greetings;
